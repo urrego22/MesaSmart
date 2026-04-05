134c5893398516a6f111011bcb0589fc986d358a
Sara Garcia <urregosara984@gmail.com>
2026-04-05 14:53:01 -0500

    Se eliminan node_modules y se agrega gitignore

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { PedidoProvider } from "./context/PedidoContext";
 import App from "./App"; // <-- Importa App, no AppRouter
